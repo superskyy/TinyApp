6407f264f0db6809aa4d4e5178ccdede7ca11915
Peter <psynevskyy.com>
2019-05-07 23:29:52 +0000

    added the short and long URL

--- express_server.js
+++ express_server.js
@@ -34,3 +34,8 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
