28af5d42fab9edeb3393c600bf2dbf1b90fc4d50
Peter <psynevskyy.com>
2019-05-07 19:46:31 +0000

    added express response.send

--- express_server.js
+++ express_server.js
@@ -7,12 +7,18 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+app.get("/", (req, res) => {
+  res.send("Hello!");
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
 
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
