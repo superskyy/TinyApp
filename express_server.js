9bd14c15eb08d4cf22e70a81290dd1034e4c0e4e
Peter <psynevskyy.com>
2019-05-07 19:45:40 +0000

    GETing the urls

--- express_server.js
+++ express_server.js
@@ -7,10 +7,12 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
