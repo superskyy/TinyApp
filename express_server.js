b40bf14b083ee2953b970730df77be4458f3c307
superskyy <PetroMac@superskyy.local>
2019-05-09 09:01:22 -0600

    refactoring, replacing var with const and adding semicolon

--- express_server.js
+++ express_server.js
@@ -1,11 +1,11 @@
-var express = require("express");
-var app = express();
-var PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 
@@ -35,7 +35,7 @@ app.get("/urls", (req, res) => {
 	urls: urlDatabase,
 	username: req.cookies["username"] 
   };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
 });
 
 app.get("/urls.json", (req, res) => {
@@ -67,33 +67,33 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 	const shortURL = req.params.shortURL;
 	delete urlDatabase[shortURL];
 	res.redirect('/urls');
-})
+});
 
 app.post('/urls/:shortURL', (req, res) => {
-	const shortURL = req.params.shortURL
-	const longURL = req.body.longURL
-	urlDatabase[shortURL] = longURL
-	res.redirect('/urls/')
-})
+	const shortURL = req.params.shortURL;
+	const longURL = req.body.longURL;
+	urlDatabase[shortURL] = longURL;
+	res.redirect('/urls/');
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
 
 app.post('/login', function (req, res) {
-  const username = req.body.username
-  res.cookie("username", username)
-  res.redirect("/urls/")
-})
+  const username = req.body.username;
+  res.cookie("username", username);
+  res.redirect("/urls/");
+});
 
 app.post('/logout', function (req, res) {
-	res.clearCookie('username')
-	res.redirect("/urls/")
-})
+	res.clearCookie('username');
+	res.redirect("/urls/");
+});
 
 function generateRandomString() {
 	const random = Math.random().toString(36).substring(2, 8);
 	return random;
-}
+};
 
 
